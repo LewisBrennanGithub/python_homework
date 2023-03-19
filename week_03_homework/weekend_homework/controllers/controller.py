eaab2a7c24d3ce4abd9cf2fb2e51986f67642850
Lewis Brennan <lewisbrennan444@gmail.com>
2023-03-19 17:54:53 +0000

    sunday commit

--- week_03_homework/weekend_homework/controllers/controller.py
+++ week_03_homework/weekend_homework/controllers/controller.py
@@ -21,13 +21,36 @@ def add_book():
   bookTitle = request.form['title']
   bookAuthor = request.form['author']
   bookGenre = request.form['genre']
-  newBook = Book(title=bookTitle, author=bookAuthor, genre=bookGenre)
+  newBook = Book(title=bookTitle, author=bookAuthor, genre=bookGenre, checked_out = False)
   add_new_book(newBook)
   return redirect('/library')
 
-@app.route("/library/<index>", methods=["POST"])
-def book_delete(index):
-    remove_named_book(int(index))
-    return redirect("/library")
+@app.route('/delete/<int:index>')
+def delete_book(index):
+    book_list.pop(index)
+    # book_list.pop(index-1)
+    return redirect('/library')
+
+@app.route('/library/<int:index>/checkout')
+def checkout_book():
+   for book in book_list:
+      book.checkout_out = True
+   return redirect('/library')
+
+# @app.route("/library/<index>", methods=["POST"])
+# def books_delete(index):
+#     delete_book(int(index))
+#     return redirect("/library")
+
+# @app.route("/library/delete/<index>", methods=["POST"])
+# def books_delete(index):
+#     remove_book(int(index))
+#     return redirect("/library")
+
+# @app.route("/library/delete/<index>", methods=["POST"])
+# def books_delete(index):
+#     chosen_book = book_list[int(index)]
+#     remove_book(chosen_book)
+#     return redirect("/library")
 
    
--- week_03_homework/weekend_homework/models/library.py
+++ week_03_homework/weekend_homework/models/library.py
@@ -1,15 +1,17 @@
 from models.book import *
 
-book1 = Book("book1", "author1", "genre1")
-book2 = Book("book2", "author2", "genre2")
-book3 = Book("book3", "author3", "genre3")
+book1 = Book("book1", "author1", "genre1", False)
+book2 = Book("book2", "author2", "genre2", False)
+book3 = Book("book3", "author3", "genre3", False)
 
 book_list = [book1, book2, book3]
 
 def add_new_book(book):
     book_list.append(book)
 
-def remove_named_book(book):
-    book_list.remove(book)
+# def change_checkout_status():
+
+
+
 
     
