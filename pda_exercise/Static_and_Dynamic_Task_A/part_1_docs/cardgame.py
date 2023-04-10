244fc5fa5c3de531a32ba838a6a361d395f8b846
Lewis Brennan <lewisbrennan444@gmail.com>
2023-04-10 19:38:18 +0100

    new commit

--- pda_exercise/Static_and_Dynamic_Task_A/part_1_docs/cardgame.py
+++ pda_exercise/Static_and_Dynamic_Task_A/part_1_docs/cardgame.py
@@ -1,20 +1,25 @@
 class CardGame:
-
   def check_for_ace(self, card):
+# On line 4 in the check_for_ace method the comparison operator should be == instead of =
     if card.value = 1:
       return True
+# On line 7 the else statement is missing a colon at the end
     else
       return False
-   
+
+#  On line 11 dif should be def and there should be a comma after card1
   dif highest_card(self, card1 card2):
   if card1.value > card2.value:
+# On line 14 the return statement should be card1
     return card
   else:
     return card2
   
 def cards_total(self, cards):
+# On line 20 the total should have an initial value
   total
   for card in cards:
     total += card.value
+# On line 24 the return statement should be properly indeneted in line with the start of the for statement. There should also be a space after "of" for presentation purposes
     return "You have a total of" + total
   
